311c8c6a8d2f39e9cb6bd3e3a636909ba93b4f0b
lia.domide <lia.domide@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2012-08-10 06:51:20 +0000

    Add 'export' command with default locale.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/docs/manuals@3007 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -3,6 +3,11 @@
 # This script should be used only to generate PDF documents during development.
 # For final distribution, there is a Python script that creates PDFs and  put them
 # in the correct place
+
+## Fir some MAC-OS systems, if default locale are not specified, rst2pdf will produce errors.
+export LC_ALL='en_US.UTF-8'
+export LANG='en_US.UTF-8'
+
 rst2pdf -o InstallationManual.pdf --stylesheets=./styles/pdf_doc.style InstallationManual.rst
 rst2pdf -o UserGuide.pdf --stylesheets=./styles/pdf_doc.style UserGuide.rst
 rst2pdf -o DeveloperReferenceManual.pdf --stylesheets=./styles/pdf_doc.style DeveloperReferenceManual.rst
